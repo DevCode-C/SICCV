c473ad09a995fbf259429d101398c65f64ff3d73
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-05-25 16:44:40 -0500

    First Commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * File:   main.c
+ * Author: Emmanuel
+ *
+ * Created on May 25, 2021, 3:49 PM
+ */
+
+
+#include "Config.h"
+#include "pic18f4550.h"
+#define _XTAL_FREQ 48000000
+
+void main(void) {
+    
+    return;
+}
