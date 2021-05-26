9559666db7ff6815396d2802220198a60ac1240d
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-05-25 22:24:41 -0500

    ADC functions added

---
+++ HeaderApp/adc.h
@@ -0,0 +1,20 @@
+#include "pic18f4550.h"
+#include "stdint.h"
+#ifndef __ADC__H
+#define __ADC__H
+    #define _XTAL_FREQ 48000000             // Definimos la frecuencia que estamos usando 
+    #define ADC_ENABLE()    ADCON0bits.ADON = 1
+    #define ADC_DISABLE()   ADCON0bits.ADON = 0;
+
+
+    /*
+        Inicializa el perifico ADC
+    */
+    void adcInit(void);
+    
+    /*
+     Retorna el valor del Puerto ADC
+    */
+    uint16_t adcGetValue(void);
+    
+#endif
